bd822df6d5e61be2e47cb349f7db900753cf6c36
burkecp <burkechristianp@gmail.com>
2019-06-28 12:39:35 -0700

    first commit

---
+++ load.py
@@ -0,0 +1,12 @@
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import cpu_count
+from tqdm import tqdm
+
+
+def threading_data(data, fn, *args, **kwargs):
+    executor = ThreadPoolExecutor(max_workers=cpu_count() * 3)
+    futures = [executor.submit(fn, d, *args, **kwargs) for d in data]
+    results = []
+    for future in tqdm(futures):
+        results.append(future.result())
+    return results
